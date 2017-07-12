071a2ce5cd84714f557cdbb54b4066635e450472
鈴木竜太 <ryota.suzuki@theport.jp>
2017-07-12 11:42:18 +0900

    入力フォームを受信

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -11,7 +11,14 @@ App.chat = App.cable.subscriptions.create("ChatChannel", {
     // Called when there's incoming data on the websocket for this channel
   },
 
-  post: function() {
+  post: function(message) {
+    window.alert(message);
     return this.perform('post');
   }
-});
+}, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
+  if (event.keyCode === 13) {
+    var chatForm = $('#chat-form');
+    App.chat.post(chatForm.val());
+    return chatForm.val('');
+  }
+}));
